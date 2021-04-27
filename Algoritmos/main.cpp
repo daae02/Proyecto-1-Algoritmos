cd7ba4e8d31725ccd8b35bb98e776668b2f7db23
daae02 <65504346+daae02@users.noreply.github.com>
2021-04-27 16:16:17 -0600

    Mejoras en la ubicacion de coincidencias

--- Algoritmos/main.cpp
+++ Algoritmos/main.cpp
@@ -1,8 +1,5 @@
 #include <iostream>
 #include "structs.cpp"
-#define SCALE 0.5 
-#define SWIDTH floor(192*SCALE) 
-#define SHEIGHT floor(108*SCALE) 
 Mat copy(Mat source, int pX, int pY){
     Rect roi(pX,pY,SWIDTH,SHEIGHT);
     Mat sliding = source(roi);
@@ -12,38 +9,38 @@ Mat rescale(Mat image){
     int width = floor(image.cols * SCALE) ;
     int height = floor(image.rows * SCALE);
     Mat resized = Mat(height,width,3);
-    resize(image,resized,resized.size(),0,0,INTER_CUBIC);
+    resize(image,resized,resized.size(),0,0,INTER_NEAREST);
     return resized;
 }
 hashMap matToHash(Mat image){
     hashMap hash = hashMap();
     int yTimes = image.rows;
     int xTimes = image.cols; 
-    for(int y = 0; y+SHEIGHT < yTimes;y++){
-        for(int x = 0; x+SWIDTH < xTimes;x++){
+    for(int y = 0; y+SHEIGHT < yTimes;){
+        for(int x = 0; x+SWIDTH < xTimes;){
             hash.insert(copy(image,x,y));
+            x +=7;
         }
+        y+=7;
     }
+
     return hash;
 }
 int compare(hashMap hash1,hashMap hash2){
     int num = 0;
     for(int i=0; i < 256; i++){
         std::cout<<"Bucket # "<<i<<std::endl;
-        num = hash1.getCoincidences(hash2,i);
-        if (num != 0){
-            std::cout<<"Coincidencias: "<< num <<std::endl;
-            return num;
-        }
+        num += hash2.getCoincidences(hash1,i);
+
     }
     std::cout<<"Coincidencias: "<< num <<std::endl;
     return 0;
 }
 int main(){
-    Mat image1 = imread("image1.jpg",1);
-    Mat image2 = imread("image2.jpg",1);
-    image1 = rescale(image1);
-    image2 = rescale(image2);
+    Mat image1 = imread("img3.jpg");
+    Mat image2 = imread("img2.jpg",1);
+    //image1 = rescale(image1);
+    //image2 = rescale(image2);
     std::cout<<"Termina reescalado"<<std::endl;
     hashMap hash1 = matToHash(image1);
     hashMap hash2 = matToHash(image2);
@@ -51,8 +48,7 @@ int main(){
     int num = compare(hash1,hash2);
     String name = "sale"+std::to_string(num)+".jpg";
     std::cout<<"Termina comparacion"<<std::endl;
-    imwrite(name,image2);
-    imshow("Prueba de que soy un genio", image1);
+    imshow("Prueba de que soy un genio", image2);
     waitKey(0);
     return 0;
 }
--- Algoritmos/structs.cpp
+++ Algoritmos/structs.cpp
@@ -1,10 +1,15 @@
 #include <opencv2/opencv.hpp>
 using namespace cv;
+#define SWIDTH floor(192*SCALE) 
+#define SHEIGHT floor(108*SCALE) 
+#define SCALE 1
 struct hashNode{
     Mat value;
+    int index;
     hashNode* next;
-    hashNode(Mat pValue){
+    hashNode(Mat pValue, int pIndex){
         value = pValue;
+        index = pIndex ;
         next = NULL;
     }
 };
@@ -16,8 +21,8 @@ struct hashBucket{
         first = last = NULL;
         cant = 0;
     }
-    void insert(Mat table){
-        hashNode* newNode = new hashNode(table);
+    void insert(Mat table, int index){
+        hashNode* newNode = new hashNode(table, index);
         if (first == NULL){
             first = last = newNode;
         }
@@ -36,47 +41,65 @@ struct hashMap{
         }
     }
     void insert(Mat table){
-        Vec3b pixelColor = table.at<cv::Vec3b>(1,1);
-        buckets[pixelColor[0]].insert(table);
+        Vec3b pixelColor = table.at<cv::Vec3b>(0,0);
+        buckets[pixelColor[0]].insert(table,buckets[pixelColor[0]].cant);
     }
-    bool matXor(Mat matSrc, Mat matDest){
-        int res;
-        int matY = floor(matSrc.rows/2);
-        int matX = matSrc.cols;
+    bool matXor(Mat matSrc, Mat matDest, int indexS, int indexD){
+        String name1, name2;
+        int matY = floor(matSrc.rows*0.30);
+        int matX = floor(matSrc.cols*0.30);
+        int coincidence = 30;
+        //int tolerace = floor((matY*matX)*0.60);
         for (int y = 0; y < matY; y++)
         {
             for (int x = 0; x < matX; x++)
             {
-                if ((matSrc.at<cv::Vec3b>(y,x)[0]^matDest.at<cv::Vec3b>(y,x)[0]) != 0 ||
-                    (matSrc.at<cv::Vec3b>(y,x)[1]^matDest.at<cv::Vec3b>(y,x)[1]) != 0 ||
-                    (matSrc.at<cv::Vec3b>(y,x)[2]^matDest.at<cv::Vec3b>(y,x)[2]) != 0 )
+                if (abs(matSrc.at<cv::Vec3b>(y,x)[0] - matDest.at<cv::Vec3b>(y,x)[0]) > coincidence ||
+                    abs(matSrc.at<cv::Vec3b>(y,x)[1] - matDest.at<cv::Vec3b>(y,x)[1]) > coincidence ||
+                    abs(matSrc.at<cv::Vec3b>(y,x)[2] - matDest.at<cv::Vec3b>(y,x)[2]) > coincidence )
                     {
-                        return false;
+                        //tolerace--;
+                        //if(tolerace == 0){
+                            return false;
+   
+                        //}
                     }
+                }
             }
-        }
+        
+        name1 = "coincs/"+std::to_string(indexS)+std::to_string(indexD)+"n1.jpg";
+        name2 = "coincs/"+std::to_string(indexS)+std::to_string(indexD)+"n2.jpg";
+        imwrite(name1,matSrc);
+        imwrite(name2,matDest);
+        std::cout<<"Coincidencia encontrada"<<std::endl;
         return true;
         
     }
     int getCoincidences(hashMap pHash, int pBucket){
         int count = 0;
-        std::cout<<"Pixeles en source: "<< this->buckets[pBucket].cant <<std::endl;
-        std::cout<<"Pixeles en dest: "<< pHash.buckets[pBucket].cant <<std::endl;
+        /*std::cout<<"Pixeles en source: "<< this->buckets[pBucket].cant <<std::endl;
+        std::cout<<"Pixeles en dest: "<< pHash.buckets[pBucket].cant <<std::endl;*/
         hashNode* tmpSrc = this->buckets[pBucket].first;
         hashNode* tmpDest = pHash.buckets[pBucket].first;
         hashNode* fnlSrc = this->buckets[pBucket].last;
         hashNode* fnlDest = pHash.buckets[pBucket].last;
-        while(tmpSrc != fnlSrc){
-            while(tmpDest != fnlDest){
-                if(matXor(tmpSrc->value,tmpDest->value))
+        int contS = this->buckets[pBucket].cant; 
+        int contD = pHash.buckets[pBucket].cant; 
+        while(tmpSrc != NULL){
+            while(tmpDest != NULL){
+                if(matXor(tmpSrc->value,tmpDest->value,contS,contD)){
                     count++;
+                    tmpSrc = tmpSrc->next;
+                }
                 tmpDest = tmpDest->next;
+                contD++;
             }
             if(count != 0){
-                return count;
+                return count;        
             }
             tmpSrc = tmpSrc->next;
+            contS++;
         }
-        return 0;
+        return count;
     }
 };
